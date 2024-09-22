447759800a22faebe311b86f89d222d2fc7e9fd6
Bohdanna Chorniy <chorniybogdanna@gmail.com>
2024-09-22 13:28:24 +0300

    Add first hometask

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+let first=50;
+let second=45;
+let suma=first+second;;
+console.log(suma);
+
+let diff=suma-50;
+console.log(diff);
+
+let prod=suma*diff;
+console.log(prod);
+
+let frac=prod/2.687;
+console.log(frac);
+
+let rem=frac%2.687;
+console.log(rem);
+
+let string='' + frac;
+console.log(string)
+
